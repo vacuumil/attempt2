cd8abce5b2a973fab544029208183f3be2af2389
vacuumil <stihoplet799@yandex.ru>
2025-09-28 02:44:46 +0400

    Оптимизировал анимации симулятора

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -32,7 +32,7 @@ export const Hero: React.FC = () => {
         </Subtitle>
         
         <CTAButtons>
-          <StyledPrimaryButton onClick={() => window.location.href = '/navigation'}>
+          <StyledPrimaryButton onClick={() => window.location.href = '../navigation'}>
             🚀 Начать обучение
           </StyledPrimaryButton>
           <StyledSecondaryButton onClick={handleLearnMore}>
